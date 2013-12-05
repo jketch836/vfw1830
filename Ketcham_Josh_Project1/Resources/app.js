0ed84f101539326214a71376ad1753de8a85d0a2
jketch836 <jketch@JoshKs-MacBook-Pro.local>
2013-12-04 23:51:15 -0500

    Ketcham_Josh_Project1_P2
    Project One, Part Two: A list of (most of) the Noble Houses of Westros.

--- Ketcham_Josh_Project1/Resources/app.js
+++ Ketcham_Josh_Project1/Resources/app.js
@@ -1,12 +1,20 @@
 
+
 //Windows
 var mainWindow = Ti.UI.createWindow({
 	title: "GoT Window",
-	backgroundImage: "silver.jpg",
+	backgroundImage: "Real-Iron-Throne-from-Game-of-Thrones-series_2.jpg",
 	backgroundRepeat: false
-	,
 });
 
+//Label
+var HouseSigilText = Ti.UI.createLabel({
+	text: "Noble Houses",
+	color: "#FFFFFF",
+	font: {fontsize: 20, fontFamily: "AmericanTypewriter"},
+	height: 40,
+	center: 0
+});
 
 //Views
 var HouseSigil = Ti.UI.createView({
@@ -26,66 +34,10 @@ var mainView = Ti.UI.createView({
 	left: 5,
 });
 
-var previous = Ti.UI.createView({
-	backgroundColor: "#545454",
-	borderRadius: 5,
-	height: Titanium.UI.SIZE,
-	bottom: 50,
-	right: 200,
-	left: 0
-});
-
-var next = Ti.UI.createView({
-	backgroundColor: "#545454",
-	borderRadius: 5,
-	height: Titanium.UI.SIZE,
-	bottom: 50,
-	right: 0,
-	left: 200
-});
-
-
-//Labels
-var HouseSigilText = Ti.UI.createLabel({
-	text: "House Sigil",
-	color: "#FFFFFF",
-	font: {fontsize: 20, fontFamily: "AmericanTypewriter"},
-	height: 40,
-	center: 0
-});
-
-var mainText = Ti.UI.createLabel({
-	text: "House Stark",
-	color: "#333",
-	font: {fontsize: 16, fontFamily: "AmericanTypewriter"},
-	height: 30,
-	center: 0
-});
-
-var previousText = Ti.UI.createLabel({
-	text: "Previous",
-	color: "#FFFFFF",
-	font: {fontsize: 14, fontFamily: "AmericanTypewriter"},
-	height: 30,
-	center: 0
-});
-
-var nextText = Ti.UI.createLabel({
-	text: "Next",
-	color: "#FFFFFF",
-	font: {fontsize: 14, fontFamily: "AmericanTypewriter"},
-	height: 30,
-	center: 0
-});
 
 
 //Main Code
-next.add(nextText);
-previous.add(previousText);
-mainView.add(mainText);
 HouseSigil.add(HouseSigilText);
 mainWindow.add(HouseSigil);
-mainWindow.add(mainView);
-mainWindow.add(next);
-mainWindow.add(previous);
 mainWindow.open();
+var theHouse= require("ArrayDisplayView");
